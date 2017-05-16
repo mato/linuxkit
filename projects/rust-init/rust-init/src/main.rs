7cf2aacbfd226e70377ef5ca322a13d5f70c7efa
Martin Lucina <martin.lucina@docker.com>
2017-05-16 16:07:33 +0200

    Try and mount /mnt
    Signed-off-by: Martin Lucina <martin.lucina@docker.com>

--- projects/rust-init/rust-init/src/main.rs
+++ projects/rust-init/rust-init/src/main.rs
@@ -1,5 +1,16 @@
+extern crate nix;
+use nix::mount::{mount, MsFlags};
+
 fn main() {
     println!("Hello, World!");
+
+    const NONE: Option<&'static [u8]> = None;
+    mount(NONE,
+          "/mnt",
+          Some(b"tmpfs".as_ref()),
+          MsFlags::empty(),
+          NONE)
+        .unwrap_or_else(|e| panic!("mount failed: {}", e));
 
     loop {
     }
